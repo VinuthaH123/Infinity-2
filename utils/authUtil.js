e242c1dba7bfd657dfd84925b5811bdd91745547
VinuthaH <vinuthavinnu123@gmail.com>
2025-12-31 14:34:26 +0530

    updating utils

--- utils/authUtil.js
+++ utils/authUtil.js
@@ -7,14 +7,16 @@ class AuthUtil {
     await page.context().clearCookies();
     await page.context().clearPermissions();
 
-    await page.goto('/login');
+    await page.goto('/login', { waitUntil: 'domcontentloaded' });
 
     await page.fill('#user_email', user.email);
     await page.fill('#password', user.password);
     await page.click('button[type="submit"]');
 
     // Wait for dashboard to load with extended timeout
-    await page.waitForURL('**/dashboard', { timeout: 120000 });
+    //await page.waitForURL('**/dashboard',  { waitUntil: 'domcontentloaded' });
+       await page.locator('.username').waitFor();
+
   }
 
   static async logout(page) {
